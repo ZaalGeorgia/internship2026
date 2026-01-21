805425655ceec6223319a9ae2e413a45515b5663
Zaal <zaal.georgia@gmail.com>
2026-01-21 18:46:35 +0400

    added moveLeft/Right

--- Tetris/src/tetris/Tetris.java
+++ Tetris/src/tetris/Tetris.java
@@ -2,8 +2,8 @@ package tetris;
 
 public class Tetris implements EventProcessor {
 
-	private static final int INITIAL_POS_X = 4;
-	private static final int INITIAL_POS_Y = 0;
+	static final int INITIAL_POS_X = 4;
+	static final int INITIAL_POS_Y = 0;
 	static final int FIGURE_SIZE = 4;
 	static final int WIDTH = 10;
 	static final int HEIGHT = 20;
@@ -22,24 +22,63 @@ public class Tetris implements EventProcessor {
 	public int posX = INITIAL_POS_X;
 	private Observer observer;
 	
+	public Tetris(Observer o) {
+		observer = o;
+	}
+
 	@Override
 	public void moveDown() {
+		if (!canMoveDown()) { // guard condition
+			return;
+		}
 		posY++;
 		observer.stateChanged();
 	}
 
-	public void addObserver(Observer o) {
-		observer = o;
+	public boolean canMoveDown() {
+		return canFigureBePlacedAt(posX, posY + 1);
 	}
 
-	public boolean canMoveDown() {
+	boolean canFigureBePlacedAt(int x, int y) {
 		for (int i = 0; i < figure.length; i++) {
 			for (int j = 0; j < figure[i].length; j++) {
-				if (figure[i][j] == 0) continue;
-				if (posY + i + 1 >= Tetris.HEIGHT) return false;
+				if (figure[i][j] == 0) {
+					continue;
+				}
+				if (y + i >= Tetris.HEIGHT) {
+					return false;
+				}
+				if (x + j < 0) {
+					return false;
+				}
+				if (x + j >= Tetris.WIDTH) {
+					return false;
+				}
 			}
 		}
 		return true;
+	}
+
+	@Override
+	public void moveLeft() {
+		if (canMoveLeft()) {
+			posX--;
+		}
+	}
+
+	@Override
+	public void moveRight() {
+		if (canMoveRight()) {
+			posX++;
+		}
+	}
+
+	public boolean canMoveLeft() {
+		return canFigureBePlacedAt(posX - 1, posY);
+	}
+
+	public boolean canMoveRight() {
+		return canFigureBePlacedAt(posX + 1, posY);
 	}
 
 }
--- Tetris/test/tetris/TetrisTest.java
+++ Tetris/test/tetris/TetrisTest.java
@@ -6,24 +6,22 @@ import org.junit.Test;
 
 public class TetrisTest {
 
+	Tetris t = new Tetris(() -> {
+	});
+
 	@Test
-	public void test() {
-		Tetris t = new Tetris();
+	public void testCreateGame() {
 		assertEquals(20, t.field.length);
 		assertEquals(10, t.field[0].length);
 		assertEquals(4, t.figure.length);
 		assertEquals(4, t.figure[0].length);
 		assertEquals(0, t.posY);
 		assertEquals(4, t.posX);
-		
-		Observer o = new Observer() {
-			@Override
-			public void stateChanged() {
-			}
-		};
-		
-		t.addObserver(o);
 
+	}
+
+	@Test
+	public void testMoveDown() {
 		assertTrue(t.canMoveDown());
 
 		EventProcessor ep = t;
@@ -32,6 +30,44 @@ public class TetrisTest {
 		
 		t.posY = Tetris.HEIGHT - Tetris.FIGURE_SIZE + 1;
 		assertFalse(t.canMoveDown());
+		
+		t.moveDown();
+		assertEquals(Tetris.HEIGHT - Tetris.FIGURE_SIZE + 1, t.posY);
+	}
+
+	@Test
+	public void testCanFigureBePlacedAt() throws Exception {
+		assertTrue(t.canFigureBePlacedAt(t.posX, t.posY));
+		assertFalse(t.canFigureBePlacedAt(-2, t.posY));
+		assertFalse(t.canFigureBePlacedAt(Tetris.WIDTH - Tetris.FIGURE_SIZE + 2, t.posY));
+	}
+
+	@Test
+	public void testCanMoveLeftAndRight() throws Exception {
+		assertTrue(t.canMoveLeft());
+		assertTrue(t.canMoveRight());
+
+		t.posX = -1;
+		assertFalse(t.canMoveLeft());
+
+		t.posX = Tetris.WIDTH - Tetris.FIGURE_SIZE + 1;
+		assertFalse(t.canMoveRight());
+	}
+
+	@Test
+	public void testMoveLeftRight() throws Exception {
+		t.moveLeft();
+		assertEquals(Tetris.INITIAL_POS_X - 1, t.posX);
+		t.moveRight();
+		assertEquals(Tetris.INITIAL_POS_X, t.posX);
+
+		t.posX = -1;
+		t.moveLeft();
+		assertEquals(-1, t.posX);
+
+		t.posX = Tetris.WIDTH - Tetris.FIGURE_SIZE + 1;
+		t.moveRight();
+		assertEquals(Tetris.WIDTH - Tetris.FIGURE_SIZE + 1, t.posX);
 	}
 
 }
